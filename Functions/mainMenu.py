c0fd2fed02aa145307d27c964916a15524a8f47d
AvitBrian <b.mugisha2@alustudent.com>
2022-10-12 12:34:32 +0200

    fresh update :)

--- Functions/mainMenu.py
+++ Functions/mainMenu.py
@@ -4,15 +4,29 @@ from rich.console import Console
 console = Console()
 
 
+
+
 def mMenu():
-    print("Choose your option")
-    print("[1] Submit a Health report")
-    print("[2] Request Health report feedback")
-    print("[3] Medical History")
-    print("[4] Exit")
-    reply = input(">>")
-    
-    
-    if (reply==1):
-        print(reply, "st Option was chosen.")
-   
+    a = 1
+    while a > 0:
+        
+        print("Choose your option");
+        print("[1] Submit a Health report");
+        print("[2] Request Health report feedback");
+        print("[3] Medical History");
+        print("[4] Exit");
+        reply = int(input(">>"));
+
+        if reply == 1:
+            print(str(reply) + "st Option was chosen.");
+        elif reply == 2:
+            print(str(reply) + "nd Option was chosen.");
+        elif reply == 3:
+            print(str(reply) + "rd Option was chosen.");
+        elif reply == 4:
+            console.print("Exiting...", style="Green");
+            time.sleep(5);
+            break;
+        else:
+            console.print("Enter a valid choice", style="Red");
+    a += a
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 from Functions import mainMenu
-from rich.console import Console 
+from rich.console import Console
+a = 1
+console = Console()
 
-console=Console()
-
-console.print("***********************************************************************************************************************");
-console.print("NobleCure");
-name= input ("Name: ");
-console.print("Welcome ",name); 
+console.print("***********************************************************************************************************************")
+console.print("NobleCure")
+name = input("Name: ")
+console.print("Welcome ", name)
 mainMenu.mMenu()
